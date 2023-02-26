3bf4d4e4044d30914ae33cb4fbe0eba89414f775
OpQueenbots <120937975+OpQueenbots@users.noreply.github.com>
2023-02-26 22:23:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,12 +21,12 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1711510822").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/OpSahubot/Vc-Player")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/OpQueenbots/Vc-Player")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/WCFnetwork")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/WorldChattingFriendsWCF")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/about_lav")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/fun_world143")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
