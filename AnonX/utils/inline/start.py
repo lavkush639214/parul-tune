6c7aa5b1d542708d56003e17b331ce6616e7c87f
OpQueenbots <120937975+OpQueenbots@users.noreply.github.com>
2023-03-14 13:09:51 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,17 +9,17 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽",
+                text="🌸 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 🌸",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="𝗛𝗲𝗹𝗽",
+                text="🌸 𝗛𝗲𝗹𝗽 🌸",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="𝗦𝗲𝘁𝘁𝗶𝗻𝗴𝘀", callback_data="settings_helper"
+                text="🌸 𝗦𝗲𝘁𝘁𝗶𝗻𝗴𝘀 🌸", callback_data="settings_helper"
             ),
         ],
      ]
@@ -30,27 +30,23 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽",
+                text="🌸 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 🌸",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="𝗛𝗲𝗹𝗽", callback_data="settings_back_helper"
+                text="🌸 𝗛𝗲𝗹𝗽 🌸", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="𝗦𝘂𝗽𝗽𝗼𝗿𝘁", url=config.SUPPORT_GROUP
+                text="🌸 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 🌸", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="𝗨𝗽𝗱𝗮𝘁𝗲𝘀", url=f"https://t.me/WCFnetwork",
+                text="🌸 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 🌸", url=f"https://t.me/Kalakar_Sangram",
             )
         ],
-        [
-            InlineKeyboardButton(
-                text="𝗦𝗼𝘂𝗿𝗰𝗲", url=f"https://t.me/Kalakar_Sangram",
-            )
-        ],
+
      ]
     return buttons
