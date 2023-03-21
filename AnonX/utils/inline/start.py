5e9bcdbf6f41ef62ab7604f83a008b8c69417204
lavkush639214 <116069475+lavkush639214@users.noreply.github.com>
2023-03-21 07:58:59 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,17 +9,17 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🌸 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 🌸",
+                text="📢 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 🔇",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌸 𝗛𝗲𝗹𝗽 🌸",
+                text="🚫 𝗛𝗲𝗹𝗽 ☣️",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="🌸 𝗦𝗲𝘁𝘁𝗶𝗻𝗴𝘀 🌸", callback_data="settings_helper"
+                text="🚮 𝗦𝗲𝘁𝘁𝗶𝗻𝗴𝘀 🚼", callback_data="settings_helper"
             ),
         ],
      ]
@@ -30,21 +30,21 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🌸 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 🌸",
+                text="🚳 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 ⛔",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌸 𝗛𝗲𝗹𝗽 🌸", callback_data="settings_back_helper"
+                text="📵 𝗛𝗲𝗹𝗽 🔞", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌸 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 🌸", url=config.SUPPORT_GROUP
+                text="⬇️ 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 🛃", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="🌸 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 🌸", url=f"https://t.me/Kalakar_Sangram",
+                text="🚻 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 🚸", url=f"https://t.me/feelings_of_lav",
             )
         ],
 
