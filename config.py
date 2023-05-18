8b88a014387a8c680118e41b84fdde72df5ed570
lavkush639214 <116069475+lavkush639214@users.noreply.github.com>
2023-05-19 00:37:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/feelings_of_lav")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/pagal_faimly")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/lavvvvvvvvvvvvvvv")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
